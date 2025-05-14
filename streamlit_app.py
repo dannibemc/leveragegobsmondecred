4a501a85320d0c171324d85c170aab6248c9777d
Danni.BE <db.mercadodecapitais@gmail.com>
2025-05-14 14:06:17 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import io
+import re
+from fpdf import FPDF
+import os
+from tempfile import NamedTemporaryFile
+import statistics
+from relatorio_envio import enviar_email, notificar_teams
+
+# (restante do conteúdo do streamlit_app completo será incluído do canvas)
+
+# Para fins de simulação, considere que este é o código final gerado e integrado
